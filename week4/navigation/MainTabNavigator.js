352409bba34bf42878e7c17257347cce1df8b613
nhat nam <namnhathoang2308@gmail.com>
2019-10-20 11:04:53 +0700

    week 4

---
+++ week4/navigation/MainTabNavigator.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import CompleteScreen from '../screens/CompleteScreen';
+import AllScreen from '../screens/AllScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import SingleTodoScreen from '../screens/SingleTodoScreen';
+
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {},
+});
+
+const CompleteStack = createStackNavigator(
+  {
+    Complete: CompleteScreen
+  },
+  config
+);
+
+CompleteStack.navigationOptions = {
+  tabBarLabel: 'Complete',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-done-all' : 'md-link'}
+    />
+  )
+};
+
+CompleteStack.path = '';
+
+const AllStack = createStackNavigator(
+  {
+    All: AllScreen,
+    SingleTodo: SingleTodoScreen
+  },
+  config
+);
+
+AllStack.navigationOptions = {
+  tabBarLabel: 'All',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
+  ),
+};
+
+AllStack.path = '';
+
+const SettingsStack = createStackNavigator(
+  {
+    Settings: SettingsScreen,
+  },
+  config
+);
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
+  ),
+};
+
+SettingsStack.path = '';
+
+const tabNavigator = createBottomTabNavigator({
+  CompleteStack,
+  AllStack,
+  SettingsStack,
+});
+
+tabNavigator.path = '';
+
+export default tabNavigator;
